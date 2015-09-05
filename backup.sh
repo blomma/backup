f2c1ed6aa06d7bf51738d0897199bc1475b0685b
Mikael Hultgren <blomma@gmail.com>
2015-09-05 14:51:00 +0200

    Cleanup to repurpose

--- backup.sh
+++ backup.sh
@@ -1,63 +1,50 @@
 #!/bin/sh
 
+if [ $UID != 0 ]; then
+	logmessage "Backup script must be invoked by root"
+	exit 1
+fi
+
 # --------------------------------------------------
 # set variables:
 # --------------------------------------------------
-mountdirectory=/Volumes/blomma-1
-backupdirectory=$mountdirectory/backup
-directoryname=ibook/"Week_"`date +%V`
+backupdirectory=/smb/maja.local/Factory/backup
+directoryname=berry/"Week_"`date +%V`
 fullbackuplabel="Full Backup of "`hostname`" on "`date '+%B %e, %Y'`
 fullbackupname=`date +%Y-%m-%d`"_full.tar.gz"
 fullbackuplogname=`date +%Y-%m-%d`"_full.log"
 incrementalbackuplabel="Incremental Backup of "`hostname`" on "`date '+%B %e, %Y'`
 incrementalbackupname=`date +%Y-%m-%d`"_incremental"`date +%H%M`".tar.gz"
 incrementalbackuplogname=`date +%Y-%m-%d`"_incremental"`date +%H%M`".log"
-filesfrom=/Users/blomma/Projects/sh/Backup/whattobackup
-excludefrom=/Users/blomma/Projects/sh/Backup/whatnottobackup
+filesfrom=~/.whattobackup
+excludefrom=~/.whatnottobackup
 tar=/usr/bin/tar
-MSMTP=/Users/blomma/Opt/bin/msmtp
-
-# Create tempfile
-tempfoo=`basename $0`
-tempfile=`mktemp -q /tmp/${tempfoo}.XXXXXX`
-if [ $? -ne 0 ]; then
-	echo "$0: Can't create temp file, exiting..."
-	exit 1
-fi
-
-# Mail
-mailsubject=""
-mailaddress="log@blomma.fastmail.fm"
 
 # --------------------------------------------------
 # functions:
 # --------------------------------------------------
 logmessage()
 {
-	echo $1 | tee -a $tempfile
+	logger $1
 }
 
 fullbackup()
 {
-	mailsubject=$fullbackuplabel
-
 	# create backup directory
 	if test ! -e $backupdirectory/$directoryname; then
-
 		logmessage "Creating $backupdirectory/$directoryname directory"
 		mkdir -p $backupdirectory/$directoryname
-
 	fi
 
 	# keep track of creation date of full backup (used with incremental backups)
 	logmessage "Updating $backupdirectory/$directoryname/lastfullbackupdate"
 	date > $backupdirectory/$directoryname/lastfullbackupdate
-	
+
 	# create backup
 	logmessage "Running tar..."
 	starttime="STARTTIME: "`date +%H:%M:%S`
 	logmessage "$starttime"
-	$tar --create  --label "$fullbackuplabel" --files-from $filesfrom --exclude-from $excludefrom --ignore-failed-read --absolute-names --verbose --gzip --file $backupdirectory/$directoryname/$fullbackupname > $backupdirectory/$directoryname/$fullbackuplogname 2>&1
+	$tar --one-file-system --create  --label "$fullbackuplabel" --files-from $filesfrom --exclude-from $excludefrom --ignore-failed-read --absolute-names --verbose --gzip --file $backupdirectory/$directoryname/$fullbackupname > $backupdirectory/$directoryname/$fullbackuplogname 2>&1
 	logmessage "$stoptime"
 	stoptime="STOPTIME: "`date +%H:%M:%S`
 	gzip $backupdirectory/$directoryname/$fullbackuplogname
@@ -66,19 +53,16 @@ fullbackup()
 	logmessage " zcat $backupdirectory/$directoryname/$fullbackuplogname"
 }
 
-
 incrementalbackup()
 {
-	mailsubject=$incrementalbackuplabel
-
 	# create variable with date of last full backup
 	lastfullbackupdatevar=`cat $backupdirectory/$directoryname/lastfullbackupdate`
-	
+
 	# check for existence of incremental backup
-	if test -e "$backupdirectory/$directoryname/$incrementalbackupname"; then 
+	if test -e "$backupdirectory/$directoryname/$incrementalbackupname"; then
 		logmessage "Your last incremental backup was less than 60 seconds ago."
 		logmessage "Wait a minute and try again."
-	else 
+	else
 		# create incremental backup
 		logmessage "Running tar..."
 		starttime="STARTTIME: "`date +%H:%M:%S`
@@ -92,21 +76,6 @@ incrementalbackup()
 		logmessage " zcat $backupdirectory/$directoryname/$incrementalbackuplogname"
 	fi
 }
-
-
-# --------------------------------------------------
-# Trap errors and cleanup after us
-# --------------------------------------------------
-trap "echo 'Interrupted...cleaning up'; rm -rf ${tempfile}; exit 1" 1 2 15
-
-
-# --------------------------------------------------
-# Mount the backup location
-# --------------------------------------------------
-if ! test -e "$backupdirectory"; then
-	/sbin/mount_smbfs //BLOMMA:'tank girl'@IAGO/BLOMMA /Volumes/blomma-1
-fi
-
 
 if test -e "$backupdirectory"; then
 	# now perform the backup.
@@ -127,19 +96,7 @@ if test -e "$backupdirectory"; then
 	fi # end if statement
 
 	logmessage "---------- Backup Script Done ----------"
-
-	cat $tempfile | $MSMTP $mailaddress
-	#cat $tempfile | mail -s "$mailsubject" $mailaddress
-
-	# Clean up
-	rm -rf ${tempfile}
-
 else
-
-	logmessage "ERROR: Couldnt mount the backup dir $backupdirectory"
+	logmessage "ERROR: Backup dir $backupdirectory doesn not exist"
 	logmessage "ERROR: Program terminated"
-
 fi
-
-#
-# End of file
